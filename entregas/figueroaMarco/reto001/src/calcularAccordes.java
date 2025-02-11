685b011c3a087e9d818c93b831d05fb5f91d8614
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2025-02-11 12:42:51 +0100

    Actualizando las metidas de pata

---
+++ entregas/figueroaMarco/reto001/src/calcularAccordes.java
@@ -0,0 +1,9 @@
+class CalcularAcordes{
+    public static void main(String[] args) { 
+        final String[] NOTAS_ESCALA = {"Do", "Do#", "Re", "Re#", "Mi", "Fa", "Fa#", "Sol", "Sol#", "La", "La#", "Si"};
+        String[] escalaAGenerar = new String[6];
+        
+
+
+    } 
+}
