5d96a1ad1d4520d4b7f4cb2c24a1ddc81c58f87d
marcof2une <marco.figueroa@alumnos.uneatlantico.es>
2025-03-11 14:06:59 +0100

    Inicializacion de programa de fracciones en clase

---
+++ entregas/figueroaMarco/reto004/Fraccion.java
@@ -0,0 +1,41 @@
+package entregas.figueroaMarco.reto004;
+
+public class Fraccion {
+
+    private int numerador;
+    private int denominador;
+        
+    public Fraccion(int numerador, int denominador){
+        assert denominador != 0 : "El denominador no puede ser 0";
+
+        int mcd = mcd(numerador, denominador);
+        this.numerador = Math.abs(numerador) / mcd;
+        this.denominador = denominador / mcd;
+    }
+
+    public Fraccion (){
+        this(0);
+    }
+
+    public Fraccion(int numerador){
+        this(numerador, 1);
+    }
+
+    public String toString(){
+        return numerador + "/" + denominador;
+    }
+
+    public Fraccion sumar(Fraccion fraccion){
+        
+    }
+
+    private int mcd(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+}
